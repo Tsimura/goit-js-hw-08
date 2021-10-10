505737f8c39659fe49a32d5f697efcabcb6a7f8a
Tsimura <chappa2828@gmail.com>
2021-10-10 20:09:20 +0300

    task -1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,62 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// const SimpleLightbox = require('simple-lightbox');
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryImagesEl = document.querySelector('div.gallery');
+const galleryImagesEl = document.querySelector('.gallery');
 const galleryMarkup = createImgGallery(galleryItems);
-let imageSource = '';
-let createModalWindow;
 
 galleryImagesEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
 galleryImagesEl.addEventListener('click', onGalleryCatchClick);
 
 function createImgGallery(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    <div class="gallery__item">
-    <a class="gallery__link" href="${original}" >
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}" 
-        alt="${description}"
-      />
-    </a>
-  </div>
-  `;
+   <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+     `;
     })
     .join('');
 }
 
 function onGalleryCatchClick(e) {
   e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
+  if (e.target.classList.contains('gallery__item')) {
     return;
   }
-  imageSource = e.target.dataset.source;
-
-  galleryImagesEl.onclick = () => {
-    createModalWindow = basicLightbox.create(
-      `
-		<img width="1400" height="900"
-    src="${imageSource}">
-	`,
-    );
-    createModalWindow.show();
-
-    window.addEventListener('keydown', onEscKeyPress);
-  };
+  //   console.log(e.target);
 }
 
-function closeModal() {
-  window.removeEventListener('keydown', onEscKeyPress);
-}
-
-function onEscKeyPress(e) {
-  if (e.code === 'Escape') {
-    createModalWindow.close();
-    closeModal();
-  }
-}
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+// console.log(galleryItems);
