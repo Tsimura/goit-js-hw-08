2773e877f95b0a74c6fcb3859de00c75eb5123dd
Yurii Tsimura <chappa2828@gmail.com>
2021-10-12 16:39:22 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,10 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {};
+const formData = {
+  email: '',
+  message: '',
+};
 const refs = {
   formEl: document.querySelector('.feedback-form'),
   emailEl: document.querySelector('input[type = "email"]'),
@@ -10,10 +13,10 @@ const refs = {
 };
 
 refs.formEl.addEventListener('input', e => {
-  // console.log(e.target.name);
+  console.log(e.target.name);
   // console.log(e.target.value);
   formData[e.target.name] = e.target.value;
-  // console.log(formData);
+  console.log(formData);
 });
 
 refs.formEl.addEventListener('submit', onFormSubmit);
@@ -28,9 +31,10 @@ function onFormSubmit(e) {
   localStorage.removeItem(STORAGE_KEY);
 }
 
+// https://www.youtube.com/watch?v=Fh8d14cY9AM&t=177s&ab_channel=GoITFrontend
 function onTextareaInput(e) {
-  const message = JSON.stringify(formData);
-  //   console.log(message);
+  let message = JSON.stringify(formData);
+  console.log(formData);
   localStorage.setItem(STORAGE_KEY, message);
 }
 
@@ -45,11 +49,11 @@ function popularTextarea() {
     // refs.messageEl.value = parsedMessage.message;
     // refs.emailEl.value = parsedMessage.email;
 
-    parsedMessage.message == undefined
+    parsedMessage.message === undefined
       ? (refs.messageEl.value = ' ')
       : (refs.messageEl.value = parsedMessage.message);
     console.log('message:', refs.messageEl.value);
-    parsedMessage.email == undefined
+    parsedMessage.email === undefined
       ? (refs.emailEl.value = ' ')
       : (refs.emailEl.value = parsedMessage.email);
     console.log('email:', refs.emailEl.value);
