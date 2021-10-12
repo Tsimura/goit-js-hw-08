2bfcc80f7f665eaf9a8f7ba87ed9c1a42b6083f1
Yurii Tsimura <chappa2828@gmail.com>
2021-10-12 10:33:31 +0300

    currentTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,17 @@
 import Player from '@vimeo/player';
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
-console.log(iframe);
+const player = new Vimeo.Player(iframe);
+const currentTime = {};
 
-// const player = new Vimeo.Player(iframe);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+player.on('play', function (currentTime) {
+  console.log('Відео відтворюється! Поточний час:', currentTime);
+  console.log(currentTime);
+});
 
 // player.getVideoTitle().then(function (title) {
 //   console.log('title:', title);
 // });
 
-// const onPlay = function (data) {
-//   // data is an object containing properties specific to that event
-//   console.log('Play!!!!');
-// };
-
-// player.on('play', onPlay);
+iframe.addEventListener('pause', on);
